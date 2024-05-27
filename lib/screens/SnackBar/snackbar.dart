e1de12eb4b298280a572592027d4cced787f1e2a
prachi_070 <149580593+prachis70@users.noreply.github.com>
2024-05-27 23:15:30 +0530

    Update snackbar.dart

--- lib/screens/SnackBar/snackbar.dart
+++ lib/screens/SnackBar/snackbar.dart
@@ -1,101 +1,59 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class bar extends StatefulWidget {
-  const bar({super.key});
+class snackBar extends StatefulWidget {
+  const snackBar({super.key});
 
   @override
-  State<bar> createState() => _barState();
+  State<snackBar> createState() => _snackBarState();
 }
 
-class _barState extends State<bar> {
+class _snackBarState extends State<snackBar> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 30,left: 10,right: 10),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              SizedBox(
-                  height: 5
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  // icon: Icon(Icons.search),
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text(
+            'Snack_Bar',
+            style: TextStyle(color: Colors.white, fontSize: 25,fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          toolbarHeight: 80,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(300, 60),
+              backgroundColor: Colors.grey,
+              padding: const EdgeInsets.all(16),
+            ),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  behavior: SnackBarBehavior.floating,
+                  content: const Text(
+                    'Open SnackBar',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  action: SnackBarAction(
+                    label: 'Retry',
+                    onPressed: () {},
                   ),
                 ),
+              );
+            },
+            child: const Text(
+              '👉Click Here',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 18,
               ),
-              SizedBox(height: 5,),
-              Wrap(
-                direction: Axis.horizontal,
-                children: [...List.generate(pr.length, (index) => box1(img: pr[index]['img']))],)],
+            ),
           ),
         ),
       ),
     );
   }
-  Padding box1({required String img}) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-          height: 180,
-          width: 150,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.grey,
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(
-                      img
-                  )
-              )
-          ),
-          child:GestureDetector(onTap: () {
-            if(favorite!=Icons.favorite)
-            {
-              setState(() {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('This Picture add to your gallry',style: TextStyle(fontSize: 18),),backgroundColor: Colors.blue.shade900,));
-              });
-            }
-            else
-            {
-              Icon(Icons.favorite_border);
-            }
-          },child: Padding(
-            padding: const EdgeInsets.only(left: 100,bottom: 100),
-            child: Icon(Icons.favorite,color: Colors.white,size:32,),
-          ))),
-
-      // ElevatedButton(onPressed: () {
-      //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('You write in page'),backgroundColor: Colors.pink,));
-      // }, child:Icon(Icons.favorite,color: Colors.white,size: 5,)),
-
-    );
-  }
 }
-List pr=[
-{
-'img':'assets/imgs/p1.jpeg',
-},
-{
-  'img':'assets/imgs/p2.jpeg',
-},
-  {
-    'img':'assets/imgs/p3.jpeg',
-  },
-  {
-    'img':'assets/imgs/p4.jpeg',
-  },
-  {
-    'img':'assets/imgs/p5.jpeg',
-  },
-  {
-    'img':'assets/imgs/p6.jpeg',
-  },
-];
-List favorite=[];
